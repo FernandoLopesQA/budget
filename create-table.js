0753e6c0ff1b1e7c1c53fb35c92257ccbd438bf5
Marcelo de Bittencourt <tchelopoa@gmail.com>
2021-03-09 23:55:04 -0300

    Main operation all working well

---
+++ create-table.js
@@ -0,0 +1,52 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'budget'
+});
+
+
+connection.connect(function(err) {
+    if (err) return console.log(err);
+    console.log('conectou!');
+    createTable(connection);
+})
+
+function createTable(conn) {
+
+    const sql = "CREATE TABLE IF NOT EXISTS Entries (" +
+        "ID INT NOT NULL AUTO_INCREMENT," +
+        "Type CHAR(1) NOT NULL," +
+        "Name VARCHAR(25) NOT NULL," +
+        "Value DECIMAL(11,2) NOT NULL," +
+        "PRIMARY KEY (ID)" +
+        ");";
+
+    conn.query(sql, function(error, results, fields) {
+        if (error) return console.log(error);
+        console.log('criou a tabela!');
+        addRows(conn)
+    });
+}
+
+function addRows(conn) {
+    const sql = "INSERT INTO Entries(Type,Name,Value) VALUES ?";
+    const values = [
+        ['R', 'Salario 1', 1500],
+        ['R', 'Salario 2', 2000],
+        ['D', 'Habitacao', 1000],
+        ['D', 'Saude', 900],
+        ['D', 'Transporte', 800],
+        ['D', 'Despesas Pessoais', 700],
+        ['D', 'Educacao', 600],
+        ['D', 'Lazer', 500],
+        ['D', 'Outros', 400]
+    ];
+    conn.query(sql, [values], function(error, results, fields) {
+        if (error) return console.log(error);
+        console.log('adicionou registros!');
+        conn.end(); //fecha a conexão
+    });
+}
---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000; //porta padrão
+const mysql = require('mysql');
+
+//configurando o body parser para pegar POSTS mais tarde
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+
+router.get('/entries', (req, res) => {
+    execSQLQuery('SELECT * FROM Entries', res);
+})
+
+router.get('/entries/:id?', (req, res) => {
+    let filter = '';
+    if (req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM Entries' + filter, res);
+})
+
+router.delete('/entries/:id', (req, res) => {
+    execSQLQuery('DELETE FROM Entries WHERE ID=' + parseInt(req.params.id), res);
+})
+
+router.post('/entries', (req, res) => {
+    const type = req.body.type.substring(0, 1);
+    const name = req.body.name.substring(0, 25);
+    const value = req.body.value.substring(0, 11);
+
+    execSQLQuery(`INSERT INTO Entries(Type, Name, Value) VALUES('${type}','${name}','${value}')`, res);
+});
+
+router.patch('/entries/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const type = req.body.type.substring(0, 1);
+    const name = req.body.name.substring(0, 25);
+    const value = req.body.value.substring(0, 11);
+
+    execSQLQuery(`UPDATE Entries SET Type='${type}', Name='${name}', Value='${value}' WHERE ID=${id}`, res);
+})
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
+
+function execSQLQuery(sqlQry, res) {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'budget'
+    });
+
+    connection.query(sqlQry, function(error, results, fields) {
+        if (error)
+            res.json(error);
+        else
+            res.json(results);
+        connection.end();
+        console.log('executou!');
+    });
+}
